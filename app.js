745fc969918fe7d5383c3cf4774199537e4f657b
Arvind <khandelwalarvind26@gmail.com>
2022-07-26 11:21:05 +0530

    Fixed a few things with POST APIs

--- app.js
+++ app.js
@@ -43,7 +43,10 @@ const mealSchema = new mongoose.Schema({
     },
     category:String,
     name:String,
-    mealItems:[{type:foodSchema}]
+    mealItems:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Food'
+    }]
 });
 const Meal = mongoose.model('Meal',mealSchema);
 
@@ -52,8 +55,14 @@ const Meal = mongoose.model('Meal',mealSchema);
 //User schemas
 const userSchema = new mongoose.Schema({
     name:String,
-    calorieRequirement:{val :{type:Number,min:1,max:100},units:{type:String,enum:['ml','liter','kg','g']}},
-    mealPlan:[mealSchema]
+    calorieRequirement:Number,
+    mealPlan:{
+        meals:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Meal'
+        }],
+        date:Number
+    }
 });
 const User = mongoose.model('User',userSchema);
 
@@ -82,18 +91,34 @@ app.get("/food",function(req,res) {
 //Rendering Meals.ejs when /meals is visited
 const category = ['Breakfast', 'Lunch', 'Evening Snack', 'Dinner'];
 app.get("/meals",function(req,res) {
-    let foodItems = [];
-    Food.find({},function(err,items){
+    Food.find({},function(err,foodItems){
         if(err)console.log(err)
         else {
-            foodItems = items;
+            Meal.find({},function(err,items) {
+                if(err)console.log(err)
+                else {
+                    res.render("meals",{items:items,foodItems:foodItems,category:category});
+                }
+            }).populate('mealItems');
         }
-    })
-    Meal.find({},function(err,items) {
-        if(err)console.log(err)
+    });
+    
+});
+
+
+
+//Rendering users.ejs when /users is visited
+app.get("/users",function(req,res) {
+
+    Meal.find({}).populate("mealItems").exec(function(err,meals) {
+        if(err) console.log(err);
         else {
-            // console.log(items);
-            res.render("meals",{items:items,foodItems:foodItems,category:category});
+            User.find({}).populate("mealPlan.meals").populate("").exec(function(err2,items) {
+                if(err2) console.log(err2);
+                else {
+                    res.render("users",{items:items,meals:meals});
+                }
+            });
         }
     });
 });
@@ -124,37 +149,13 @@ app.post("/food",function(req,res){
 
 //Handling post request to /meal when a new item is added
 app.post("/addMeals",function(req,res){
-    const arr = [];
-    
-    const id = req.body.checked;
-    // console.log(id);
-    if(id.size === 1) {
-        Food.findById(id,function(err,food) {
-            if(err) console.log(err);
-            else {
-                console.log(food.name);
-                arr.push(food);
-            }
-        });
-    }
-    else {
-        id.forEach(function(id){
-            Food.findById(id,function(err,food) {
-                if(err) console.log(err);
-                else {
-                    console.log(food.name);
-                    arr.push(food);
-                }
-            });
-        });
-    }
     const curr = new Meal({
         name:req.body.name,
         validCategory:req.body.category,
         category:req.body.category,
-        mealItems:arr
+        mealItems:req.body.checked
     });
-    console.log(curr.mealItems);
+    // console.log(curr.mealItems);
     curr.save(function(err) {
         if(err)console.log(err);
     });
@@ -162,6 +163,48 @@ app.post("/addMeals",function(req,res){
 });
 
 
+//Handling post request on /addUsers to add new users
+app.post("/addUsers",function(req,res){
+    const curr = new User({
+        name:req.body.name,
+        calorieRequirement:req.body.calReq,
+        mealPlan:{
+            date:req.body.date,
+            meals:req.body.checked
+        }
+    });
+    curr.save(function(err) {
+        if(err)console.log(err);
+        else console.log("Success");
+    });
+    res.redirect('/users');
+})
+
+
+
+//Handle post requeusts /mealspatch to patch meals with patch api
+// app.post("/mealsPatch",function(req,res) {
+//     console.log(req.body);
+//     const id = req.body.checked;
+//     Food.find({},function(err,foodItems){
+//         if(err)console.log(err)
+//         else {
+//             Meal.findById(id,function(err,item) {
+//                 if(err)console.log(err)
+//                 else {
+//                     res.render("mealsPatch",{item:item,foodItems:foodItems,category:category});
+//                 }
+//             }).populate('mealItems');
+//         }
+//     });
+// });
+
+
+
+//Handle patch request to /patchMeal
+// app.post("/patchMeal",function(req,res) {
+//     console.log(req.body);
+// });
 
 //Setting up the server to listen on localhost:3000/
 app.listen(3000,function(){console.log("Server started on port 3000")});
